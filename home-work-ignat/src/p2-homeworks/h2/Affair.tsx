075166a7ca77dfa3bda239d5b34774fe4eccd926
Timofey <buzmakov1987@gmail.com>
2021-04-09 21:12:29 -0700

    H1 and H2 done

--- home-work-ignat/src/p2-homeworks/h2/Affair.tsx
+++ home-work-ignat/src/p2-homeworks/h2/Affair.tsx
@@ -1,21 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { AffairType } from './HW2';
+import c from './Affairs.module.css';
+import { isPropertyAccessExpression } from 'typescript';
 
 type AffairPropsType = {
-    // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
-}
+  // key не нужно типизировать
+  affair: AffairType;
+  deleteAffairCallback: (_id: number) => void;
+};
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
+  const deleteCallback = () => {
+    props.deleteAffairCallback(props.affair._id);
+  };
 
-    return (
-        <div>
-            // show some text
+  let rating;
+  switch (props.affair.priority) {
+    case 'low':
+      rating = c.low;
+      break;
+    case 'middle':
+      rating = c.middle;
+      break;
+    case 'high':
+      rating = c.high;
+      break;
+    default:
+      break;
+  }
 
-            <button onClick={deleteCallback}>X</button>
-        </div>
-    )
+  //   let rating = c + props.affair.priority;
+  console.log(rating);
+
+  return (
+    <div className={c.container}>
+      <div className={c.titleContainer}>
+        <span>{props.affair.name}</span>
+      </div>
+
+      <div className={c.priorityContainer}>
+        <span className={rating}>[{props.affair.priority}]</span>
+      </div>
+
+      <div className={c.deleteButtonContainer}>
+        <button onClick={deleteCallback}>X</button>
+      </div>
+    </div>
+  );
 }
 
-export default Affair
+export default Affair;
