a11e6516183611fc80147b79f0eb876515c80a3d
buzmakovTim <buzmakov1987@gmail.com>
2021-05-08 18:10:43 -0700

    All 5 HW done

--- home-work-ignat/src/p2-homeworks/h5/HW5.tsx
+++ home-work-ignat/src/p2-homeworks/h5/HW5.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import Header from './Header'
 import Routes from './Routes'
+import {HashRouter} from 'react-router-dom'
+// import s from '../h5/Header.module.css'
 
 function HW5() {
     return (
-        <div>
+        <div> 
             {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
+            <HashRouter>
 
             <Header/>
 
             <Routes/>
 
-            {/*</HashRouter>*/}
+            </HashRouter>
         </div>
     )
 }
--- home-work-ignat/src/p2-homeworks/h5/Header.tsx
+++ home-work-ignat/src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import { PATH } from './Routes'
+import s from './Header.module.css'
 
 function Header() {
     return (
+        <div className={s.header}> 
         <div>
-            // add NavLinks
-
+            <NavLink to={PATH.PRE_JUNIOR} className={s.link} activeClassName={s.active}>Pre-Junior</NavLink>
+            <NavLink to={PATH.JUNIOR} className={s.link} activeClassName={s.active}>Junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS} className={s.link} activeClassName={s.active}>Junior+</NavLink>
+        </div>
+            <div className={s.block}/>
         </div>
     )
 }
